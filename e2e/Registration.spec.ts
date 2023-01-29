1387568afe7f82964cdcbf4a9f24e8fffac00dee
Володя <igor185097@gmail.com>
2023-01-29 15:49:56 +0300

    changed registrationPage.ts

--- e2e/Registration.spec.ts
+++ e2e/Registration.spec.ts
@@ -2,17 +2,17 @@ import {test} from "@playwright/test";
 import {RegistrationPage} from "../pages/registration/registrationPage";
 
 
-test('Register', async ({page}) =>{
-
+test('Register true email', async ({page}) =>{
     await test.step('Fill correct data ' , async () =>{
         const registrationPage = new RegistrationPage(page)
         await registrationPage.register('igor185097@gmail.com' , 'aaaaaaaa', 'aaaaaaaa')
         await registrationPage.fillCorrectData()
     })
+})
+test('Register wrong email', async ({page}) =>{
     await test.step('fillWrongEmailAndToShortPasswordAndAnotherConfirm',async() =>{
         const registrationPage = new RegistrationPage(page)
         await registrationPage.register('dsfg','sDadc','reg')
         await registrationPage.fillWrongEmailAndToShortPasswordAndAnotherConfirm()
     })
-
 })
